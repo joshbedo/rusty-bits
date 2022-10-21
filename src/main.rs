feb1c9b873bc9e6b5b54a1fbb61a720d2e13dbe6
josh <josh@Jesses-MacBook-Pro.local>
2022-10-21 15:08:48 -0400

    convert string input to i32 to match against secret number

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+    
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -11,5 +16,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("you guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
