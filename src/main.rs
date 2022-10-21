e6fe4b80b82d1a704f2274814ff04469836fb0d5
josh <josh@Jesses-MacBook-Pro.local>
2022-10-21 15:17:14 -0400

    add error handling for non-numeric input and break loop when you guess the right number

--- src/main.rs
+++ src/main.rs
@@ -17,14 +17,20 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("you guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
         }
     }
 }
